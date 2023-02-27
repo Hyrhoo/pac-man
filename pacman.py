d9ee8a417a5bb7722dea16c5300bfbb0ea907ea6
Hyrhoo <hyrhooo@gmail.com>
2023-02-27 19:34:17 +0100

    première version

--- pacman.py
+++ pacman.py
@@ -1,165 +1,69 @@
-"""
-Programme réalisé par nom, prénom, classe
-"""
 import pygame
 
-#variables du niveau
-NB_TILES = 27   #nombre de tiles a chager (ici de 00.png à 26.png) 27 au total !!
-TITLE_SIZE=32   #definition du dessin (carré)
-largeur=8       #hauteur du niveau
-hauteur=8       #largeur du niveau
-tiles=[]       #liste d'images tiles
+    # ==== variables ==== #
 
-#variables de gestion du pacman
-pacX=1          #position x y du pacman dans le niveau
-pacY=1
-compteurBilles=0
+GLOBAL_FPS = 60     # FPS global du jeu
+TILE_SIZE = 32      # definition du dessin (carré)
+length = 28         # hauteur du niveau
+height = 31         # largeur du niveau
+tiles = []          # liste d'images tiles
 
-#variables de gestion du fantome
-FRAMERATE_FANTOME= 120      #vitesse du fantome chiffre elevé = vitesse lente
-NB_DEPLACEMENT_FANTOME =9   #le fantome se deplace sur 9 cases
-positionFantome=1
-frameRateCounterFantome=0
-posfX=4     #position initiale du fantome
-posfY=2
+    # ==== classes ==== #
 
-#definition du niveau
+class Labyrinth:
+    pass
 
-niveau=[[1,5,5,5,5,5,5,2],
-     [6,0,12,12,12,12,12,6],
-     [6,12,12,12,12,12,12,6],
-     [6,12,12,12,12,12,12,6],
-     [6,12,12,12,12,12,12,6],
-     [6,12,12,12,12,12,12,6],
-     [6,12,12,12,12,12,12,6],
-     [3,5,5,5,5,5,5,4]]
+class Pac_man:
+    pass
 
-fantome=[[0,0,0,0,0,0,0,0],
-     [0,0,0,0,0,0,0,0],
-     [0,0,0,0,1,2,3,0],
-     [0,0,0,0,8,0,4,0],
-     [0,0,0,0,7,6,5,0],
-     [0,0,0,0,0,0,0,0],
-     [0,0,0,0,0,0,0,0],
-     [0,0,0,0,0,0,0,0]]
+class Ghost:
+    pass
 
+    # ==== fonctions ==== #
 
-#la taille de la fenetre dépend de la largeur et de la hauteur du niveau
-#on rajoute une rangée de 32 pixels en bas de la fentre pour afficher le score d'ou (hauteur +1)
-pygame.init()
-fenetre = pygame.display.set_mode((largeur*TITLE_SIZE, (hauteur+1)*TITLE_SIZE))
-pygame.display.set_caption("Pac-Man")
-font = pygame.font.Font('freesansbold.ttf', 20)
+def loadtile(file):
+   return pygame.transform.scale(pygame.image.load(file), (TILE_SIZE, TILE_SIZE))
 
-def chargetiles(tiles):
-    """
-    fonction permettant de charger les images tiles dans une liste tiles[]
-    """
-    for n in range(0,NB_TILES):
-        #print('data/'+str(n)+'.png')
-        tiles.append(pygame.image.load('data/'+str(n)+'.png')) #attention au chemin
-
-
-def afficheNiveau(niveau):
+def drawlevel(niveau):
     """
     affiche le niveau a partir de la liste a deux dimensions niveau[][]
     """
-    for y in range(hauteur):
-        for x in range(largeur):
-            fenetre.blit(tiles[niveau[y][x]],(x*TITLE_SIZE,y*TITLE_SIZE))
+    for y in range(height):
+        for x in range(length):
+            screen.blit(tiles[niveau[y][x]], (x * TILE_SIZE, y * TILE_SIZE))
+    pygame.draw.line(screen, "#FFFFFF", (length * TILE_SIZE, 0), (length * TILE_SIZE, height * TILE_SIZE))
 
+def load_map(file):
+    with open(file, "r") as f:
+        labyrinth = []
+        for line in f.readlines():
+            labyrinth.append([int(i) for i in line.strip("\n").split()])
+        return labyrinth
 
-def affichePac(numero):
-    """
-    affiche le pacman en position pacX et pacY
-    """
-    fenetre.blit(tiles[numero],(pacX * TITLE_SIZE,pacY * TITLE_SIZE))
+    # ==== init ==== #
 
-def afficheScore(score):
-    """
-    affiche le score
-    """
-    scoreAafficher = font.render(str(score), True, (0, 255, 0))
-    fenetre.blit(scoreAafficher,(120,250))
+for n in range(18):
+    tiles.append(loadtile(f"data/{n}.png"))
+img_pacman = loadtile("data/pacman.png")
+imp_ghost = loadtile("data/ghost.png")
+map = load_map("map.txt")
 
-def rechercheFantome(fantome,position): #recherche les coord du fantome dans la liste fantome
-    """
-    recherche les coordonnées du fantome en fonction du numéro de sa postion dans le parcours
-    """
-    print(position)                     #la position doit etre dans la liste fantome sinon plantage
-    for y in range(hauteur):
-        for x in range(largeur):
-            if fantome[y][x]==position:
-                coodFantome=x,y
-    return coodFantome          #les coord du fantome x et y sont dans un tuple coodFantome
+screen = pygame.display.set_mode((TILE_SIZE * (length + 10), TILE_SIZE * height))
+clock = pygame.time.Clock()
 
-def deplaceFantome(fantome):
-    """
-    Incrémente automatiquement le déplacement du fantome, gère sa vitesse et son affichage
-    """
-    global frameRateCounterFantome
-    global positionFantome
-    global posfX,posfY
-    if frameRateCounterFantome==FRAMERATE_FANTOME:      #ralenti la viteese du fantome
-        posfX,posfY=rechercheFantome(fantome,positionFantome)   #deballage du tuple coordonnées du fantome
-        positionFantome+=1
-        if positionFantome==NB_DEPLACEMENT_FANTOME:     #un tour est fait donc on passe à la 1ere position
-            positionFantome=1
-        frameRateCounterFantome=0                       #compteur de vitesse à zero
-    fenetre.blit(tiles[15],(posfX * TITLE_SIZE,posfY * TITLE_SIZE)) #affichage du fantome
-    frameRateCounterFantome+=1                          #incrémentation du compteur de vitesse
+    # ==== main loop ==== #
 
-
-chargetiles(tiles)  #chargement des images
-
-loop=True
-while loop==True:
+run = True
+while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            loop = False            #fermeture de la fenetre (croix rouge)
-        elif event.type == pygame.KEYDOWN:  #une touche a été pressée...laquelle ?
-            if event.key == pygame.K_UP:    #est-ce la touche UP
-                posY = pacY - 1             #on deplace le pacman vituellement
-                posX = pacX
-                numeroTile = niveau[posY][posX]       #on regarde le numéro du tile
-                print("up",numeroTile,end=':')
-                if (numeroTile == 12 or numeroTile == 0):   #si le tile est une bille ou un fond noir alors le déplacement est possible
-                    pacY -= 1                               #on monte donc il faut décrémenter pacY
-                    print("deplacement possible",pacX,pacY)
-                else:
-                    print("deplacement impossible")
-            elif event.key == pygame.K_DOWN:  #est-ce la touche DOWN
-                posY = pacY + 1
-                posX = pacX
-                numeroTile = niveau[posY][posX]
-                print("down",numeroTile,end=':')
-                if (numeroTile == 12 or numeroTile == 0):
-                    pacY += 1
-                    print("deplacement possible",pacX,pacY)
-                else:
-                    print("deplacement impossible")
-            elif event.key == pygame.K_RIGHT:  #est-ce la touche RIGHT
-                pass
-                #A compléter pour le déplacement à droite
+            run = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                run = False
+    
+    drawlevel(map)
+    pygame.display.flip()
+    clock.tick(GLOBAL_FPS)
 
-            elif event.key == pygame.K_LEFT:  #est-ce la touche LEFT
-                pass
-                #A compléter pour le déplacement à gauche
-
-            elif event.key == pygame.K_ESCAPE or event.unicode == 'q': #touche q pour quitter
-                loop = False
-            if (numeroTile==12):  #si le numero du tile est 12 c'est que l'on est sur une nouvelle bille
-                compteurBilles+=1   #alors on incrémente le score
-                niveau[posY][posX]=0    #et on efface la bille dans le niveau
-                print("nouvelle bille")
-            else:
-                print("fond noir")
-
-    fenetre.fill((0,0,0))   #efface la fenetre
-    afficheNiveau(niveau)   #affiche le niveau
-    affichePac(14)          #affiche la pacman et le score
-    deplaceFantome(fantome) #mettre un commentaire pour desactiver le déplacement du fantome
-    afficheScore(compteurBilles)
-    pygame.display.update() #mets à jour la fentre graphique
 pygame.quit()
-
