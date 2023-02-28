108e7efd19ad8262742eb06d7656736ddb20c2c8
Hyrhoo <hyrhooo@gmail.com>
2023-02-28 22:16:47 +0100

    les classe c'est fait pour faire de la programmation orienter OBJET

--- pacman.py
+++ pacman.py
@@ -4,45 +4,69 @@ import pygame
 
 GLOBAL_FPS = 60     # FPS global du jeu
 TILE_SIZE = 32      # definition du dessin (carré)
+NUMBRE_IMG = 18     # nombre d'images à charger
 length = 28         # hauteur du niveau
 height = 31         # largeur du niveau
-tiles = []          # liste d'images tiles
 
     # ==== classes ==== #
 
 class Labyrinth:
     
-    def __init__(self, file):
-        self.map = load_map(file)
+    def __init__(self, map_file):
+        """
+        initialise les données de la classe en chargeant d'abord les images puis les datas de la map
+
+        Args:
+            map_file (str): le nom du fichier à charger
+        """
+        self.load_tiles()
+        self.load_map(map_file)
+
+    def load_tiles(self):
+        """
+        charge les images nécessaires à l'affichage du labyrinthe
+        """
+        self.tiles = []
+        for n in range(NUMBRE_IMG):
+            self.tiles.append(pygame.transform.scale(pygame.image.load(f"data/{n}.png").convert(), (TILE_SIZE, TILE_SIZE)))
+    
+    def load_map(self, file):
+        """
+        charge le fichier contenant les datas de la map
+
+        Args:
+            file (str): le nom du fichier à charger
+        """
+        with open(file, "r") as f:
+            labyrinth = []
+            for line in f.readlines():
+                labyrinth.append([int(i) for i in line.strip("\n").split()])
+        self.map = labyrinth
     
     def drawlevel(self):
         """
-        affiche le niveau a partir de la liste a deux dimensions niveau[][]
+        affiche le niveau a partir de la liste a deux dimensions self.map[][]
         """
         for y, ligne in enumerate(self.map):
             for x, case in enumerate(ligne):
-                screen.blit(tiles[case], (x * TILE_SIZE, y * TILE_SIZE))
+                screen.blit(self.tiles[case], (x * TILE_SIZE, y * TILE_SIZE))
         pygame.draw.line(screen, "#FFFFFF", (length * TILE_SIZE, 0), (length * TILE_SIZE, height * TILE_SIZE))
 
+
 class Pac_man:
-    pass
+    
+    def __init__(self) -> None:
+        self.img = pygame.transform.scale(pygame.image.load("data/pacman.png").convert(), (TILE_SIZE, TILE_SIZE))
+
 
 class Ghost:
-    pass
+    
+    def __init__(self) -> None:
+        self.img = pygame.transform.scale(pygame.image.load("data/ghost.png").convert(), (TILE_SIZE, TILE_SIZE))
 
     # ==== fonctions ==== #
 
-def loadtile(file):
-   img = pygame.image.load(file).convert()
-   return pygame.transform.scale(img, (TILE_SIZE, TILE_SIZE))
 
-
-def load_map(file):
-    with open(file, "r") as f:
-        labyrinth = []
-        for line in f.readlines():
-            labyrinth.append([int(i) for i in line.strip("\n").split()])
-        return labyrinth
 
     # ==== init ==== #
 
@@ -50,12 +74,9 @@ pygame.init()
 screen = pygame.display.set_mode((TILE_SIZE * (length + 10), TILE_SIZE * height))
 clock = pygame.time.Clock()
 
-for n in range(18):
-    tiles.append(loadtile(f"data/{n}.png"))
-img_pacman = loadtile("data/pacman.png")
-imp_ghost = loadtile("data/ghost.png")
-
 labyrinth = Labyrinth("map.txt")
+player = Pac_man()
+ghost = Ghost()
 
     # ==== main loop ==== #
 
